2281cd36636187e5afdcf64d30f68b9c46526a7e
joshuajy03 <52546898+joshuajy03@users.noreply.github.com>
2021-06-24 12:14:53 -0700

    changed capitalization

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Welcome from "./components/welcome.jsx";
-import PatientSignIn from "./components/patientsignin.jsx";
+import Welcome from "./components/Welcome.jsx";
+import PatientSignIn from "./components/PatientSignIn.jsx";
 
 
 const Stack = createStackNavigator();
