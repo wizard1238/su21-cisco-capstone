25812d2aa4d365cb9aba3733cdbf31caaa27fc8b
unknown <joshuajy03@gmail.com>
2021-06-28 14:12:26 -0700

    homepage

--- patient home/components/PatientHome.jsx
+++ patient home/components/PatientHome.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import 'react-native-gesture-handler';
 import { StyleSheet, Text, View, } from 'react-native';
 import PatientProfile from "./PatientProfile.jsx";
-import { Button, Title } from 'react-native-paper';
+import { Button, Title, Searchbar } from 'react-native-paper';
 import { Header } from "react-native/Libraries/NewAppScreen";
 import { createStackNavigator } from '@react-navigation/stack';
+
 
 const Stack = createStackNavigator();
 
@@ -17,9 +18,37 @@ function PatientHome({ navigation }) {
                     <Text style={{ marginTop: 55, textAlign: 'center', fontWeight: 'bold', fontSize: 24, marginLeft: 21 }}> Hello, Joshua </Text>
                 </View>
                 <View style={styles.container}>
-                    <Title>Upcoming Appointments</Title>
-                    <Title>Messages</Title>
-                    <Title>Alerts</Title>
+                    <Searchbar placeholder="Search" />
+                    <Title style={styles.title}>Upcoming Appointments</Title>
+                    <View style={styles.table}>
+                        <Text>Date | Description</Text>
+                    </View>
+                    <View style={styles.table}>
+                    <Text>Date | Description</Text>
+                    </View>
+                    <View style={styles.table}>
+                    <Text>Date | Description</Text>
+                    </View>
+                    <Title style={styles.title}>Messages</Title>
+                    <View style={styles.table}>
+                        <Text>Name | Description</Text>
+                    </View>
+                    <View style={styles.table}>
+                        <Text>Name | Description</Text>
+                    </View>
+                    <View style={styles.table}>
+                        <Text>Name | Description</Text>
+                    </View>
+                    <Title style={styles.title}>Alerts</Title>
+                    <View style={styles.table}>
+                        <Text>Alert</Text>
+                    </View>
+                    <View style={styles.table}>
+                        <Text>Alert</Text>
+                    </View>
+                    <View style={styles.table}>
+                        <Text>Alert</Text>
+                    </View>
                 </View>
 
             </View>
@@ -29,11 +58,22 @@ function PatientHome({ navigation }) {
 
 }
 
+
 const styles = StyleSheet.create({
     title: {
         textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: 18,
+        marginTop: 10,
+        marginBottom: 10,
+        fontSize: 20,
+    },
+    table: {
+        borderWidth: 2,
+        width: "60%",
+        justifyContent: 'center',
+        display: 'flex',
+        marginLeft: "20%",
+        marginBottom: 1,
+
     },
     container: {
         backgroundColor: "#dce8e6",
@@ -43,7 +83,8 @@ const styles = StyleSheet.create({
         backgroundColor: "#cbe6f6",
         width: "100%",
         flexDirection: 'row',
-    }
+    },
+
 });
 
 export default PatientHome;
