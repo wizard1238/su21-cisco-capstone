beeb17f4d1f746b20c11b329a510a7c7ceca24d4
unknown <joshuajy03@gmail.com>
2021-06-28 11:26:48 -0700

    title material design

--- part 2/components/PatientHome.jsx
+++ part 2/components/PatientHome.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 import PatientProfile from "./PatientProfile.jsx";
-import { Button } from 'react-native-paper';
+import { Button, Title } from 'react-native-paper';
 import { Header } from "react-native/Libraries/NewAppScreen";
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-function PatientHome( {navigation} ) {
+function PatientHome({ navigation }) {
     return (
         <React.Fragment>
-        <View>
-        <View style={styles.header}>
-        <Button icon="menu" labelStyle={{ fontSize: 40 }} style={{ marginTop: 50, marginLeft: 8}} color={"black"}  onPress={() => navigation.openDrawer()} />
-        <Text style={{marginTop: 55, textAlign: 'center', fontWeight: 'bold', fontSize: 24, marginLeft: 21}}> Hello, Joshua </Text>
-        </View>
-        <View style={styles.container}>
-        <Text style={styles.title}>Upcoming Appointments</Text>
-        <Text style={styles.title}>Messages</Text>
-        <Text style={styles.title}>Alerts</Text>
-        </View>
+            <View>
+                <View style={styles.header}>
+                    <Button icon="menu" labelStyle={{ fontSize: 40 }} style={{ marginTop: 50, marginLeft: 8 }} color={"black"} onPress={() => navigation.openDrawer()} />
+                    <Text style={{ marginTop: 55, textAlign: 'center', fontWeight: 'bold', fontSize: 24, marginLeft: 21 }}> Hello, Joshua </Text>
+                </View>
+                <View style={styles.container}>
+                    <Title>Upcoming Appointments</Title>
+                    <Title>Messages</Title>
+                    <Title>Alerts</Title>
+                </View>
 
-        </View>
+            </View>
         </React.Fragment>
     );
 
