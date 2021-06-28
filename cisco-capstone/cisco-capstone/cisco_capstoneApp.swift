368d5285e73571fdb0f1a1644a19f4174cf42c9b
simfony <49770335+wizard1238@users.noreply.github.com>
2021-06-28 12:33:09 -0700

    created new xcode project

---
+++ cisco-capstone/cisco-capstone/cisco_capstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  cisco_capstoneApp.swift
+//  cisco-capstone
+//
+//  Created by Jeremy Tow on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct cisco_capstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
