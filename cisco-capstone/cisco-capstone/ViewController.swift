bac9d7f6d5c4ca60e8860f8de6ff84b97b1e31fb
simfony <49770335+wizard1238@users.noreply.github.com>
2021-06-28 12:42:16 -0700

    switched project to uikit

---
+++ cisco-capstone/cisco-capstone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cisco-capstone
+//
+//  Created by Jeremy Tow on 6/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
